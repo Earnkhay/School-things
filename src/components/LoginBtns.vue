f0e1a1494649d288c288295f42adeff10acb58d2
Julia <earnkay2@gmail.com>
2023-03-10 18:28:40 +0100

    added icons

--- src/components/LoginBtns.vue
+++ src/components/LoginBtns.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        <button-comp>{{ pageType == "signUp" ? "Sign Up with Google" : "Sign In with Google"}}</button-comp>
-        <button-comp>{{ pageType == "signUp" ? "Sign Up with Facebook" : "Sign In with Facebook"}}</button-comp>
+    <div class="text-sm sm:text-lg">
+        <button-comp class="flex justify-center items-center">
+            <img src="../assets/icons8-google-48.png" class="mr-2" width="20" height="25" alt="">
+            {{ pageType == "signUp" ? "Sign Up with Google" : "Sign In with Google"}}
+        </button-comp>
+        <button-comp class="flex justify-center items-center">
+            <i class="fa-brands fa-facebook text-blue-600 mr-2"></i>
+            {{ pageType == "signUp" ? "Sign Up with Facebook" : "Sign In with Facebook"}}
+        </button-comp>
     </div>
 </template>
 
